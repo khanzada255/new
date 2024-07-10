820779612d4842362b66545833d0361b509bdd00
khanzada255 <laraibkhanzada51@gmail.com>
2024-07-10 19:31:51 +0500

    Add files via upload

---
+++ text.py
@@ -0,0 +1,16 @@
+a = int (input('Enter first number'))
+b = int (input('Second number'))
+c = (input('Operator'))
+if c == "+":
+    print (a + b)
+elif c == "-":
+    print(a - b)
+elif c == "/":
+    print(a / b)
+elif c == "*":
+    print(a * b)
+else:
+    print("error")
+  
+
+
